ae8894c38e237ed775578760538691dd31971384
nirbhay24 <nirbhaymishra24@gmail.com>
2018-05-19 22:37:16 +0530

    MultiThreading Pattern

---
+++ CoreJava/src/coreJava/multithreading/PrintPatternByThread.java
@@ -0,0 +1,70 @@
+package coreJava.multithreading;
+
+public class PrintPatternByThread {
+
+	static Object monitor = new Object();
+
+	static boolean a = true;
+	static boolean b = false;
+
+	public static void main(String[] args) {
+
+		Thread t1 = new Thread(new SequenceDisplayImpl('a'));
+		Thread t2 = new Thread(new SequenceDisplayImpl('b'));
+		t1.start();
+		t2.start();
+
+	}
+
+	static class SequenceDisplayImpl implements Runnable {
+		char threadId;
+		SequenceDisplayImpl(char threadId) {
+			this.threadId = threadId;
+		}
+
+		public void run() {
+			print();
+		}
+
+		
+		
+		private void print() {
+			try {
+				while (true) {
+					Thread.sleep(500);
+					synchronized (monitor) {
+						
+						
+						if ('a' == threadId) {
+							if (!a) {
+								monitor.wait();
+							} else {
+								System.out.print(threadId + " ");
+								a = false;
+								b = true;
+								monitor.notifyAll();
+							}
+						}
+						
+						
+						if ('b' == threadId) {
+							if (!b) {
+								monitor.wait();
+							} else {
+								System.out.print(threadId + " ");
+								a = true;
+								b = false;
+								monitor.notifyAll();
+							}
+						}
+					}
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		
+	}
+}
